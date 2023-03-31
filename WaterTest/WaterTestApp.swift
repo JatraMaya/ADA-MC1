f59ecd3952b43d152423e065454f246109fa5cd9
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-03-31 15:02:56 +0700

    Initial Commit

---
+++ WaterTest/WaterTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTestApp.swift
+//  WaterTest
+//
+//  Created by Ahmad Fadly Iksan on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WaterTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
